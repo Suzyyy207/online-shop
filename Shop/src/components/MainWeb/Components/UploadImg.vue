c491fa2e61f0fa0bbcfbe0deb0cb4cd5ca05330e
wanghelin215 <1148291406@qq.com>
2023-03-31 13:10:53 +0800

    feat(front):uploadImg add API /setUserAvatar to post img only

--- Shop/src/components/MainWeb/Components/UploadImg.vue
+++ Shop/src/components/MainWeb/Components/UploadImg.vue
@@ -1,7 +1,8 @@
 <!--用户头像组件-->
 <template>
   <div>
-    <img v-if="this.isUploaded" :src="getIcon(this.imageUrl)">
+    <!--img v-if="this.isUploaded" :src="getIcon(this.imageUrl)"-->
+    <img v-if="this.isUploaded" :src="this.imageUrl">
     <input
       v-if="!this.isUploaded"
       type="file"
@@ -22,40 +23,53 @@ export default {
     };
   },
   created() {
-    //this.getUserInfo();
+    this.getUserAvatar();
   },
   methods: {
     getUserAvatar: function() {
       var localStorage = window.localStorage;
+
       this.$axios.post('/getUserAvatar', {
-        username: localStorage.getItem("username")
+        // 测试版本：
+        username: "数据库存储的username名称"
+        // 正式版本：username: localStorage.getItem("username")
       })
       .then(res => {
         if(res.data.state==3) {
-          this.imageUrl = URL.createObjectURL(response.data);
-          this.isUploaded=true;
+          // this.imageUrl = URL.createObjectURL(res.data.data);
+          // this.isUploaded=true;
+          this.$message.success("收到反馈（根据是否显示图片判断文件传输是否成功）");
+          const formData = res.data.avatar;
+          const blob = formData.get('image');
+          const imgUrl = URL.createObjectURL(blob);
+          // const img = document.createElement('img');
+          // img.src = imgUrl;
+          this.imageUrl = imgUrl;
+          this.isUploaded = true;
         }
         else {
           this.isUploaded=false;
         }
       })
     },
-    getIcon: function (name) {
-      return new URL(`../../../assets/${name}`, import.meta.url).href;
-    },
+    //getIcon: function (name) {
+    //  return new URL(`../../../assets/${name}`, import.meta.url).href;
+    //},
     addFile: function(file) {
       var files = file.target.files[0];
       console.log(files.name)
+      console.log(files)
 
-      var formData = new FormData();
-      formData.append('/setUserAvatar', file.target.files[0])
-      this.$axios.post('/upload', formData, {
+      var avatar = new FormData();
+      avatar.append('image', file.target.files[0])
+      this.$axios.post('/setUserAvatar', avatar, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       }).then(res => {
         if(res.data.state == 3) {
           this.$message.success("上传成功");
+          // this.getUserAvatar();
         }
         else {
           this.$message.error(res.data.message);
