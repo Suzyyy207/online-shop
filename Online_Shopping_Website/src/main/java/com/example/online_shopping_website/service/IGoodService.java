2e94119784788d03ce3fb7029a9671fc17c32bc8
Shi HongXiang <228753522@qq.com>
2023-04-09 20:04:23 +0800

    fix mergefail

--- Online_Shopping_Website/src/main/java/com/example/online_shopping_website/service/IGoodService.java
+++ Online_Shopping_Website/src/main/java/com/example/online_shopping_website/service/IGoodService.java
@@ -2,7 +2,14 @@ package com.example.online_shopping_website.service;
 
 import com.example.online_shopping_website.entity.Good;
 
+import java.util.List;
+
 public interface IGoodService {
     Good goodsRegister(String introduction,String goodsname,float goodsPrice,int goodsStock,String goodsCategory,String shopname);
     void setGoodsInformation(String introduction,String goodsname,float goodsPrice,int goodsStock,String goodsCategory,String shopname,int goodsId);
+    void picsUpload(int goodsId,byte[] pic);
+    List<Good> searchByStatus(String shopname,int status);
+    List<Good> searchByRegisterStatus(String shopname,int registerStatus);
+    List<Good> searchByModifyStatus(String shopname,int modifyStatus);
+
 }
