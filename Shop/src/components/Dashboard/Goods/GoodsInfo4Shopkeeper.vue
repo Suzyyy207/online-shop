f3f329f0a70c13a279bf989fefe5729b2919d944
yukexin <20300750093@fudan.edu.cn>
2023-04-13 11:44:09 +0800

    fix(front): ShopManage layout changed

--- Shop/src/components/Dashboard/Goods/GoodsInfo4Shopkeeper.vue
+++ Shop/src/components/Dashboard/Goods/GoodsInfo4Shopkeeper.vue
@@ -1,76 +1,93 @@
 <!-- 商家查看商品详情 -->
 <template>
-    <div>
-        <el-button @click="cancelShowGoodsDetail">返回</el-button>
+    <div class="wrap">
+        <h1>进度表</h1>
 
         <!-- 进度条 -->
-        <el-card>
+        <el-card class="steps">
             <el-steps :active="activeIndex">
                 <el-step title="基本信息填写"></el-step>
                 <el-step title="管理员审核"></el-step>
                 <el-step title="申请成功"></el-step>
                 <el-step title="商品已下架"></el-step>
             </el-steps>
-
         </el-card>
 
-        <el-form :model="addForm" :rules="addFormRules" ref="addForm" label-width="100px" label-position="top">
+        <el-form class="form" :model="addForm" :rules="addFormRules" ref="addForm">
             <div v-if="this.activeIndex==1">
-               <GoodsRegister :goods='goods'/>
-               <el-button 
-                    v-if="!(goods.registerStatus==2 || goods.modifyStatus == 2)"
-                    type="primary" 
-                    @click="this.getGoodsInfo()" 
-                >取消
-                </el-button>
+                <div class="btns">
+                    <el-button 
+                        class="btn"
+                        v-if="!(goods.registerStatus==2 || goods.modifyStatus == 2)"
+                        type="primary" 
+                        @click="this.getGoodsInfo()" 
+                    >取消
+                    </el-button>
+                    <el-button class="btn" @click="cancelShowGoodsDetail">返回管理页</el-button>
+                </div>
+                
+               <GoodsRegister class="reg" :goods='goods'/>
+               
             </div>
 
             <div v-if="this.activeIndex==2">
-                管理员正在审核，请耐心等待...
+                <el-space class="hint">管理员正在审核，请耐心等待...</el-space>
                 <GoodsDetail :goods="this.goods"/>
-
-                    <el-button 
+                <div class="btns">
+                    <el-button
+                        class="btn" 
                         type="primary" 
                         @click="this.modifyGoodsInfo()" 
                         >修改商品信息
                     </el-button>
-                    <el-button 
+                    <el-button
+                        class="btn" 
                         type="primary" 
                         @click="this.cancelApplication()" 
                         >撤销申请
                     </el-button>
-                
+                    <el-button class="btn" @click="cancelShowGoodsDetail">返回</el-button>
+                </div>
             </div>
 
             <div v-if="this.activeIndex==3">
                 <GoodsDetail :goods="this.goods"/>
-
-                    <el-button 
+                <div class="btns">
+                    <el-button
+                        class="btn" 
                         type="primary" 
                         @click="this.modifyGoodsInfo()" 
                         >修改商品信息
                         </el-button>
-                        <el-button 
+                        <el-button
+                            class="btn" 
                             type="primary" 
                             @click="this.goodsOffShelve()" 
                         >下架
                     </el-button>
-                
+                    <el-button class="btn" @click="cancelShowGoodsDetail">返回</el-button>
+                </div>
             </div>
             <div v-if="this.activeIndex==4">
                 <GoodsDetail :goods="this.goods"/>
 
                 <div v-if="showType==1">
-                    <el-button 
-                        type="primary" 
-                        @click="this.goodsOnShelve()" 
-                        >重新上架
-                    </el-button>
+                    <div class="btns">
+                        <el-button
+                            class="btn" 
+                            type="primary" 
+                            @click="this.goodsOnShelve()" 
+                            >重新上架
+                        </el-button>
+                        <el-button class="btn" @click="cancelShowGoodsDetail">返回</el-button>
+                    </div>
                 </div>
 
                 
             </div>
         </el-form>
+
+        
 
 
     </div>
@@ -194,11 +211,49 @@ export default {
 
  
 <style scoped>
- .el-steps{
-  margin: 15px 0;
+.wrap{
+    margin:10px;
+    padding:30px;
+    background-color: #fff;
+    border-radius: 15px;
+    color: #303133;
+    border: 3px solid #ebeef5;
+    transition: .3s;
 }
-.el-step__title{
-  font-size: 13px;
+.wrap h1{
+  text-align: left;
+  font-family:"Lucida Console", "Courier New", monospace;
+  font-size: 30px;
+  margin-bottom:20px;
+  font-weight: bolder;
+  color:#81A18B;
 }
 
+.steps{
+    margin-bottom:30px;
+}
+
+.form{
+    line-height: 30px;
+}
+
+.btns{
+    display: flex;
+    margin-top:20px;
+    gap:20px;
+}
+.btn{
+    width:100%;
+}
+
+.hint{
+    margin-left: 20px;
+    font-family: "Brush Script MT", cursive;
+    font-size: 20px;
+
+}
+
+.reg{
+    margin-top: 40px;
+}
 </style>
